7bce9502b7ff2399e50c38f4f5857fb7f531c501
“mac” <kni2012@ukr.net>
2019-08-11 18:01:50 +0300

    start working on blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,26 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '/translation.json');
 }
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
+];
+
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, BlogComponent],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
@@ -31,4 +39,4 @@ export function createTranslateLoader(http: HttpClient) {
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
