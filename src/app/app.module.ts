5264cee52aae2ef7c56c2099e99c4b52792d377b
“mac” <kni2012@ukr.net>
2019-08-12 16:02:41 +0300

    add 18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BlogComponent } from './blog/blog.component';
 
+import { TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
+// let current_language = 'ru';
+import { RU_TRANS } from './messages.ru';
+
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '/translation.json');
 }
@@ -36,7 +40,11 @@ const appRoutes: Routes = [
       }
     })
   ],
-  providers: [],
+  providers: [
+    { provide: TRANSLATIONS, useValue: RU_TRANS },
+    { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
+    // { provide: LOCALE_ID, useValue: this.current_language }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/messages.ru.ts
@@ -0,0 +1,9 @@
+export const RU_TRANS = ` <?xml version="1.0" encoding="UTF-8" ?>
+<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
+  <file source-language="en" datatype="plaintext" original="ng2.template">
+    <body>
+    <h1>sdfgsdfg</h1>
+    </body>
+  </file>
+</xliff>
+`;
